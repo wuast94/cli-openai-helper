8ec1b81fec30b65793ece4ed37fb0d387445e078
wuast94 <mihauku@googlemail.com>
2023-11-26 01:37:10 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# cli-openai-helper
-Tool to convert Comments in shell to a usable command 
+# My Zsh Plugin
+
+This is a Zsh plugin that provides ...
+
+## Installation
+
+To install this plugin, clone this repository into your `$ZSH_CUSTOM/plugins` directory:
+
+```sh
+git clone https://github.com/wuast94/cli-openai-helper.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/cli-openai-helper
+Usage
+After installation, add cli-openai-helper to the plugins array in your .zshrc file:
+
+
+Copy
+
+Open
+plugins=(... cli-openai-helper)
+Restart your terminal or source your .zshrc file to apply the changes:
+
+
+Copy
+
+Open
+source ~/.zshrc
+Features
+Feature 1
+Feature 2
+...
+License
+This project is licensed under the MIT License - see the LICENSE.md file for details.
---
+++ create_completion.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import openai
+import sys
+import os
+import configparser
+
+# Get config dir from environment or default to ~/.config
+CONFIG_DIR = os.getenv('XDG_CONFIG_HOME', os.path.expanduser('~/.config'))
+API_KEYS_LOCATION = os.path.join(CONFIG_DIR, 'openaiapirc')
+
+# Read the secret_key from the ini file ~/.config/openaiapirc
+# The format is:
+# [openai]
+# secret_key=<your secret key>
+# model=gpt-4-1106-preview  # Optional: Defaults to gpt-4-1106-preview if not provided
+
+def create_template_ini_file():
+    """
+    If the ini file does not exist create it and add the secret_key and optionally the model
+    """
+    if not os.path.isfile(API_KEYS_LOCATION):
+        with open(API_KEYS_LOCATION, 'w') as f:
+            f.write('[openai]\n')
+            f.write('secret_key=\n')
+            f.write('model=gpt-4-1106-preview\n')
+
+        print('OpenAI API config file created at {}'.format(API_KEYS_LOCATION))
+        print('Please edit it and add your secret key')
+        print('If you do not yet have a secret key, you need to register for OpenAI API access.')
+        sys.exit(1)
+
+def initialize_openai_api():
+    """
+    Initialize the OpenAI API
+    """
+    # Check if file at API_KEYS_LOCATION exists
+    create_template_ini_file()
+    config = configparser.ConfigParser()
+    config.read(API_KEYS_LOCATION)
+
+    openai.api_key = config['openai']['secret_key'].strip('"').strip("'")
+
+    model = config['openai'].get('model', 'gpt-4-1106-preview').strip('"').strip("'")
+
+    return model
+
+model = initialize_openai_api()
+
+cursor_position_char = int(sys.argv[1])
+
+# Read the input prompt from stdin.
+buffer = sys.stdin.read()
+prompt_prefix = buffer[:cursor_position_char]
+prompt_suffix = buffer[cursor_position_char:]
+full_command = prompt_prefix + prompt_suffix
+
+# Add additional parameters such as temperature and max_tokens as desired
+response = openai.Completion.create(
+    model=model,
+    prompt=[
+        {"role": "system", "content": "You are a zsh shell expert, please help me complete the following command, you should only output the completed command, no need to include any other explanation."},
+        {"role": "user", "content": full_command}
+    ],
+    temperature=0.5,  # Change this value to adjust randomness
+    max_tokens=150,   # Change this value to adjust the maximum length of the completion
+    n=1,              # Number of completions to generate
+    stop=None,        # Sequence where the API should stop generating further tokens
+    user="zsh-user"   # This can be set to any string that identifies the user
+)
+
+completed_command = response.choices[0].text.strip()
+
+sys.stdout.write(f"\n{completed_command.replace(prompt_prefix, '', 1)}")
