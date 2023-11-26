8159c58165fdb556da68b1d1279063e5cf83abd1
wuast94 <mihauku@googlemail.com>
2023-11-26 04:41:37 +0100

    7

--- create_completion.py
+++ create_completion.py
@@ -77,6 +77,6 @@ response = client.chat.completions.create(
     user="zsh-user",
 )
 
-completed_command = response['choices'][0]['message']['content']
+completed_command = response.choices[0].message.content.strip()
 
 sys.stdout.write(f"\n{completed_command.replace(prompt_prefix, '', 1)}")
