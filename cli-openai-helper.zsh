f20dc4e8af44d27f7444e15f5a6e9d20dfd15929
wuast94 <mihauku@googlemail.com>
2023-11-26 02:32:13 +0100

    v0.2

--- cli-openai-helper.zsh
+++ cli-openai-helper.zsh
@@ -6,12 +6,27 @@
 create_completion() {
     # Check if the buffer starts with a hashtag.
     if [[ $BUFFER =~ '^# ' ]]; then
+        # Debug output
+        echo "Buffer before processing: $BUFFER" >&2
+
         # Remove the hashtag from the command.
         local nl_command=${BUFFER:2}
+
+        # Debug output
+        echo "Natural language command to process: $nl_command" >&2
+
         # Call the Python script with the natural language command.
         local completion=$(echo -n "$nl_command" | $ZSH_CUSTOM/plugins/zsh_codex/create_completion.py $CURSOR)
+
+        # Debug output
+        echo "Generated command from Python script: $completion" >&2
+
         # Replace the buffer with the generated command.
         BUFFER=$completion
+
+        # Debug output
+        echo "Buffer after processing: $BUFFER" >&2
+
         # Set the cursor position at the end of the buffer.
         CURSOR=$#BUFFER
     fi
